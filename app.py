dbe0f436dee2180b26f50db43df6854135eacfc2
redihoxha <redi.hoxha@y42.com>
2023-05-24 18:03:08 +0200

    Add endpoints.

--- app.py
+++ app.py
@@ -20,5 +20,21 @@ def about(station, date):
             "date": date,
             "temperature": temperature}
 
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+@app.route("/api/v1/annual/<station>/<year>")
+def year_data(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["    DATE"] = df["    DATE"].astype(str).str[:4].astype(int)
+    df = df.loc[df["    DATE"] == int(year)]
+    result = df.to_dict(orient="records")
+    return result
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5011)
